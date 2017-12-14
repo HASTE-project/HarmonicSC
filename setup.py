fbd5961f76dbde4b8ccea74acd092754549db43c
Ben Blamey <blamey.ben@gmail.com>
2017-12-14 07:00:31 +0100

    add py_modules to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='harmonicSC',
       version='0.1.0',
       install_requires=['urllib3'],
+      py_modules=['stream_connector']
       )
